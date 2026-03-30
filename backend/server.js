521e026f71c290844b9f5995c019f09a2f76c26e
Yash <goofebib123@gmail.com>
2026-03-30 16:25:48 +0530

    fix duplicate bcrypt

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,6 @@ if (data.users.length === 0) {
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const initializeDatabase = require('./database');
 
 const app = express();
